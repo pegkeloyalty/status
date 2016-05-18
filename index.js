204b7f05cc631ad9443edd9935293d5508f8da1d
niksmac <nikhil@lightrains.com>
2016-05-18 11:10:03 +0530

    Added the Logic

---
+++ index.js
@@ -0,0 +1,27 @@
+'use strict';
+var later = require('later'),
+    Twitter = require('twitter'),
+    request = require('superagent');
+require('dotenv').config();
+var client = new Twitter({
+  consumer_key: process.env.consumer_key,
+  consumer_secret: process.env.consumer_secret,
+  access_token_key: process.env.access_token_key,
+  access_token_secret: process.env.access_token_secret,
+});
+later.date.localTime();
+var statApi = process.env.PEGKEAPI;
+var textSched = later.parse.text('every 4 sec');
+var timer2 = later.setInterval(logTime, textSched);
+function logTime() {
+  request
+    .get(statApi)
+    .end(function(err, res){
+      var d = res.body.last_mod;
+      var sstatus = res.body.sstatus.API;
+      var theTweet = "All systems " + sstatus + " #PegkeStat Last updated on:" + d + " https://pegke.com/about/system-status";
+      client.post('statuses/update', {status: theTweet},  function(error, tweet, response){
+        if(error) throw error;
+      });
+  });
+}
